94d6da6b024a92d3525a40e832e675f08772a328
Jorge Bou-saad <jbousing@gmail.com>
2017-11-10 20:36:18 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,19 @@
-var config = {
+/*var config = {
     apiKey: "AIzaSyCMtjdf0RDkRHQnqhqCOMundi9EQkVADr8",
     authDomain: "somosdb-9de21.firebaseapp.com",
     databaseURL: "https://somosdb-9de21.firebaseio.com",
     projectId: "somosdb-9de21",
     storageBucket: "somosdb-9de21.appspot.com",
     messagingSenderId: "846508767839"
+  };
+  firebase.initializeApp(config); */
+
+var config = {
+    apiKey: "AIzaSyDnv9EXITwHisZFk-VsTJbKt-3WKbazRw8",
+    authDomain: "pruebafirebase-ad270.firebaseapp.com",
+    databaseURL: "https://pruebafirebase-ad270.firebaseio.com",
+    projectId: "pruebafirebase-ad270",
+    storageBucket: "pruebafirebase-ad270.appspot.com",
+    messagingSenderId: "1022183260336"
   };
   firebase.initializeApp(config);
